ab90a9ac1e55a7cc1a8507fed45f75bc4b15e6e0
Matthew Kenkel <mattsnoopy2@gmail.com>
2024-09-27 14:15:42 -0400

    Enabling nvim config copy

--- home/user/source.nix
+++ home/user/source.nix
@@ -4,7 +4,7 @@ in
   home.file = {
       ".config/hypr".source = "${cfg}/hypr";
       ".config/alacritty".source = "${cfg}/alacritty";
-      # ".config/nvim".source = "${cfg}/nvim";
+      ".config/nvim".source = "${cfg}/nvim";
       # ".config/wallpapers".source = "${cfg}/wallpapers";
       # ".config/kitty".source = "${cfg}/kitty";
       # ".config/neofetch".source = "${cfg}/neofetch";
