498aa170b29b320ec6a8a209b9b0f6592bbc278d
Matthew Kenkel <mattsnoopy2@gmail.com>
2024-09-26 21:54:33 -0400

    dir chg

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
         modules = [
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
-          ./nixos/configuration.nix
+          ./core/configuration.nix
         ];
       };
   };
