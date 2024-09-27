c7668509eff12384f5ace8ff2b47a2307596e831
Matthew Kenkel <mattsnoopy2@gmail.com>
2024-09-26 22:06:30 -0400

    hyprland

---
+++ core/modules/hyprland.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }: {
+
+  programs.hyprland = {
+    # Install the packages from nixpkgs
+    enable = true;
+    # Whether to enable XWayland
+    xwayland.enable = true;
+  };
+  # ...
+
+}
