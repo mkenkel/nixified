ad8233235e827854108c5a2d914f573ffa664b13
Matthew Kenkel <mattsnoopy2@gmail.com>
2024-09-27 11:19:52 -0400

    Adding hyprland as conf file

--- home/default.nix
+++ home/default.nix
@@ -19,4 +19,20 @@
     xwayland.enable = true;
     systemd.enable = true;
   };
+
+
+
+  home.file."~/.config/hypr/hyprland.conf".text = ''
+    decoration {
+      shadow_offset = 0 5
+      col.shadow = rgba(00000099)
+    }
+
+    $mod = SUPER
+
+    bindm = $mod, mouse:272, movewindow
+    bindm = $mod, mouse:273, resizewindow
+    bindm = $mod ALT, mouse:272, resizewindow
+  '';
+
 }
