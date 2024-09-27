fd96bb7914a3581b121133eae2a7da01cd68e814
Matthew Kenkel <mattsnoopy2@gmail.com>
2024-09-27 09:46:22 -0400

    home-manager init

--- flake.nix
+++ flake.nix
@@ -4,10 +4,17 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+  inputs.home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-  outputs = { self, nixpkgs , ...} @ inputs: {
+  outputs = { self, nixpkgs, home-manager, ...} @ inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
     nixosConfigurations.upshot = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
         specialArgs = { inherit inputs; }; # this is the important part (Hyprland)
         modules = [
         # Import the previous configuration.nix we used,
@@ -15,5 +22,17 @@
           ./core/configuration.nix
         ];
       };
-  };
+
+    homeConfigurations."matt" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      # Specify your home configuration modules here, for example,
+      # the path to your home.nix.
+      modules = [ ./home ];
+      # Optionally use extraSpecialArgs
+      # to pass through arguments to home.nix
+      };
+    };
+
+
+
 }
