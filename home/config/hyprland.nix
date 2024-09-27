a7ae4a54491f390baf009f86de9287f7399152d1
Matthew Kenkel <mattsnoopy2@gmail.com>
2024-09-27 10:49:32 -0400

    Whoops, forgot to enable home-manager's hyprland

--- home/config/hyprland.nix
+++ home/config/hyprland.nix
@@ -1,5 +1,17 @@
 { config, pkgs, ... }: 
 {
+  wayland.windowManager.hyprland = {
+    # Whether to enable Hyprland wayland compositor
+    enable = true;
+    # The hyprland package to use
+    package = pkgs.hyprland;
+    # Whether to enable XWayland
+    xwayland.enable = true;
+    # Optional
+    # Whether to enable hyprland-session.target on hyprland startup
+    systemd.enable = true;
+  };
+
   wayland.windowManager.hyprland.settings = {
     decoration = {
       shadow_offset = "0 5";
@@ -7,6 +19,8 @@
     };
 
     "$mod" = "SUPER";
+    "$terminal" = "alacritty";
+    "$menu" = "wofi --show drun";
 
     bindm = [
       # mouse movements
