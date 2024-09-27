e90e14c1f1d9e2bdf4997d658e7e1a4f0c64a66e
Matthew Kenkel <mattsnoopy2@gmail.com>
2024-09-27 15:11:10 -0400

    Removed gunk

--- home/user/zsh.nix
+++ home/user/zsh.nix
@@ -3,8 +3,6 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
 
     shellAliases = {
       ll = "ls -l";
