b6799b298731903643bd7ddaa8b738845073a6d0
Matthew Kenkel <mattsnoopy2@gmail.com>
2024-09-27 15:22:17 -0400

    Jeez

--- home/user/zsh.nix
+++ home/user/zsh.nix
@@ -2,15 +2,10 @@
 {
   programs.zsh = {
     enableCompletion = true;
-    autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
 
-    shellAliases = {
-      ll = "ls -l";
-      update = "sudo nixos-rebuild switch";
-    };
     history = {
-      size = 10000;
+      save = 10000;
       path = "${config.xdg.dataHome}/zsh/history";
     };
   };
