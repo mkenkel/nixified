830745176adfc7a0a9636f4db6c8027b1878588c
Matthew Kenkel <mattsnoopy2@gmail.com>
2024-09-27 09:12:52 -0400

    Troubleshooting

--- core/modules/hyprland.nix
+++ core/modules/hyprland.nix
@@ -1,10 +1,8 @@
-{ inputs, config, pkgs, ... }: {
+{ inputs, pkgs, ... }: {
 
   programs.hyprland = {
     # Install the packages from nixpkgs
     enable = true;
-    # Whether to enable XWayland
-    xwayland.enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     # make sure to also set the portal package, so that they are in sync
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
