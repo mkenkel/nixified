a1ac3e3990cf21e89097b680d5e27cbf1d123cea
Matthew Kenkel <mattsnoopy2@gmail.com>
2024-09-27 15:17:57 -0400

    moving stuff until it works

--- core/modules/user.nix
+++ core/modules/user.nix
@@ -1,5 +1,5 @@
-{ config, lib, pkgs, ... }: {
-
+{ config, lib, pkgs, ... }:
+{
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.matt = {
     isNormalUser = true;
@@ -10,17 +10,5 @@
   # Leaving this here for later, but idk where else to put it rn
   programs.zsh = {
     enable = true;
-    enableCompletion = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
-
-    shellAliases = {
-      ll = "ls -l";
-      update = "sudo nixos-rebuild switch";
-    };
-    history = {
-      size = 10000;
-      path = "${config.xdg.dataHome}/zsh/history";
-    };
   };
-  }
+}
--- home/user/zsh.nix
+++ home/user/zsh.nix
@@ -1,3 +1,17 @@
 { pkgs, config, ... }:
 {
+  programs.zsh = {
+    enableCompletion = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      ll = "ls -l";
+      update = "sudo nixos-rebuild switch";
+    };
+    history = {
+      size = 10000;
+      path = "${config.xdg.dataHome}/zsh/history";
+    };
+  };
 }
