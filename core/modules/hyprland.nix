675084d54e0d912e57c0f291a346e4d6f7c883d7
Matthew Kenkel <mattsnoopy2@gmail.com>
2024-09-27 09:15:47 -0400

    Screw it - trying both removed at this point

--- core/modules/hyprland.nix
+++ core/modules/hyprland.nix
@@ -3,9 +3,6 @@
   programs.hyprland = {
     # Install the packages from nixpkgs
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    # make sure to also set the portal package, so that they are in sync
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
     environment.systemPackages = with pkgs; [
