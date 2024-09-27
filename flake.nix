d80685c4f11a89b4a6f0c2e29556e5d26a9a1b85
Matthew Kenkel <mattsnoopy2@gmail.com>
2024-09-27 09:00:00 -0400

    Actually learning the flake nix at this point - Hyprland updates

--- flake.nix
+++ flake.nix
@@ -2,15 +2,13 @@
 {
   description = "Flake for Upshot - my desktop env.";
 
-  inputs = {
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-unstable";
-    };
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs , ...} @ inputs: {
     nixosConfigurations.upshot = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+        specialArgs = { inherit inputs; }; # this is the important part (Hyprland)
         modules = [
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
