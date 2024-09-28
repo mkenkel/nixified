bca5b12bdc463462bfe6e8b9479a49d72810af3c
Matthew Kenkel <mattsnoopy2@gmail.com>
2024-09-28 09:24:17 -0400

    podman re-arrange

--- home/user/podman.nix
+++ home/user/podman.nix
@@ -15,9 +15,11 @@
   };
 
   # Useful other development tools
-  environment.systemPackages = with pkgs; [
-    dive # look into docker image layers
-    podman-tui # status of containers in the terminal
-    podman-compose # start group of containers for dev
+  home.packages = [
+    pkgs.dive # look into docker image layers
+    pkgs.podman
+    pkgs.podman-tui # status of containers in the terminal
+    pkgs.podman-desktop
+    pkgs.podman-compose
   ];
 }
