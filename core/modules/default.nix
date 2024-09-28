f1ee7bea32fa58d845777852484148aa8935474d
Matthew Kenkel <mattsnoopy2@gmail.com>
2024-09-28 10:29:45 -0400

    virt - not podman

--- core/modules/default.nix
+++ core/modules/default.nix
@@ -4,12 +4,12 @@
   imports = [
     ./fonts.nix
     ./hyprland.nix
-    ./podman.nix
     ./programs.nix
     ./services.nix
     ./syscfg.nix
     ./systemPackages.nix
     ./user.nix
+    ./virtualization.nix
   ];
 
   nixpkgs = {
