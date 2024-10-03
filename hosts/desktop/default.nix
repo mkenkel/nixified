236b2f1a935cd69231a941e9b6e04139001f3564
whipplash <mattsnoopy2@gmail.com>
2024-10-03 16:09:55 -0400

    changed indent

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -30,7 +30,6 @@
    boot = {
     loader = {
       efi.canTouchEfiVariables = true;
-
       grub = {
         efiSupport = true;
         enable = true;
@@ -60,34 +59,34 @@
 
 ### Hyprland.nix ###
 
-    environment.systemPackages = with pkgs; [
-      xdg-desktop-portal-gtk
-      xdg-desktop-portal-wlr
-      fuzzel
-      hyprpaper
-      hypridle
-      hyprlock
-      hyprcursor
-      kitty
-      libnotify
-      mako
-      qt5.qtwayland
-      qt6.qtwayland
-      wl-clipboard
-      waybar
-      # Terminal
-      vim 
-      wget
-      neovim
-      git
-      unzip
-      tmux
-      # Virtualization/Containerization
-      podman
-      podman-compose
-      qemu
-      virtiofsd
-    ];
+  environment.systemPackages = with pkgs; [
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-wlr
+    fuzzel
+    hyprpaper
+    hypridle
+    hyprlock
+    hyprcursor
+    kitty
+    libnotify
+    mako
+    qt5.qtwayland
+    qt6.qtwayland
+    wl-clipboard
+    waybar
+    # Terminal
+    vim 
+    wget
+    neovim
+    git
+    unzip
+    tmux
+    # Virtualization/Containerization
+    podman
+    podman-compose
+    qemu
+    virtiofsd
+  ];
 
 ### Programs.nix ###
 
