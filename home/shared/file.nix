c8925e58d55f1881bf4bb30988ff79a46b95a623
Matthew Kenkel <matt@mktogo.fatnutfarm.com>
2024-10-05 14:52:39 -0400

    Fastfetch customization

--- home/shared/file.nix
+++ home/shared/file.nix
@@ -6,5 +6,6 @@ in
   home.file = {
       ".config/alacritty".source = "${cfg}/alacritty";
       ".config/nvim".source = "${cfg}/nvim";
+      ".config/fastfetch".source = "${cfg}/fastfetch";
   };
 }
