d84e0eebb728ea3bb2cc56a7dd9d520b6caaf73a
whipplash <mattsnoopy2@gmail.com>
2024-10-11 16:27:47 -0400

    Changing greeter from Hyprlock to tuigreet

--- nix/nixos/desktop/hyprland.nix
+++ nix/nixos/desktop/hyprland.nix
@@ -35,6 +35,15 @@ in
   #     default_session = initial_session;
   #   };
   # };
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        user = "greeter";
+      };
+    };
+  };
 
   environment.systemPackages = with pkgs; [
     fuzzel
