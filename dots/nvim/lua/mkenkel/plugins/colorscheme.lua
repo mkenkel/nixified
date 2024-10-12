ef6190dd8df1e0cfb2144007a24b58bdca5d2254
whipplash <mattsnoopy2@gmail.com>
2024-10-12 12:36:51 -0400

    Catpuccin or however it's spelled

--- dots/nvim/lua/mkenkel/plugins/colorscheme.lua
+++ dots/nvim/lua/mkenkel/plugins/colorscheme.lua
@@ -1,6 +1,14 @@
 return {
-  "sainnhe/sonokai",
-  config = function()
-    vim.cmd("colorscheme sonokai")
+  -- "sainnhe/sonokai",
+  -- config = function()
+  --   vim.cmd("colorscheme sonokai")
+  -- end,
+  
+  --
+"catppuccin/nvim",
+name = "catppuccin",
+priority = 1000, 
+  config = function ()
+    vim.cmd("colorscheme catppuccin")
   end,
 }
