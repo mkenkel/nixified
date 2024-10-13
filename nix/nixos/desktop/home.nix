25296dcb8d03273b9d014ffbaf4471179e2ee22e
whipplash <mattsnoopy2@gmail.com>
2024-10-13 14:30:02 -0400

    huh

--- nix/nixos/desktop/home.nix
+++ nix/nixos/desktop/home.nix
@@ -264,7 +264,6 @@ in
       dwindle = {
         pseudotile = true;
         preserve_split = true;
-        no_gaps_when_only = 0;
         smart_split = false;
         smart_resizing = false;
       };
