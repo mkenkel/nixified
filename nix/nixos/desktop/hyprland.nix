af7f86b4aa2c01cd9bcc710cc318a5bf03305093
whipplash <mattsnoopy2@gmail.com>
2024-10-13 14:15:59 -0400

    at this point let me keel over... this is horrible

--- nix/nixos/desktop/hyprland.nix
+++ nix/nixos/desktop/hyprland.nix
@@ -17,8 +17,6 @@
     trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
   };
 
-  home.homeDirectory = "/home/matt";
-
   home.packages = [
     pkgs.catppuccin-cursors.mochaMauve
     pkgs.fuzzel
