afaa9ee8997a0e7ba843795700b7ebfbbfbcfe74
whipplash <mattsnoopy2@gmail.com>
2024-10-13 17:20:06 -0400

    Isolating stylix - new WP

--- nix/nixos/desktop/default.nix
+++ nix/nixos/desktop/default.nix
@@ -19,6 +19,7 @@ in
 
   imports = [
     ../shared
+    ./stylix.nix
     ./hardware-configuration.nix
   ];
 
@@ -40,36 +41,6 @@ in
   nixpkgs = {
     config = {
       allowUnfree = true;
-    };
-  };
-
-  stylix = {
-    enable = true;
-    image = ../../../dots/wallpaper/mocha-nix.png;
-    polarity = "dark";
-    opacity.terminal = 0.8;
-    cursor.package = pkgs.bibata-cursors;
-    cursor.name = "Bibata-Modern-Ice";
-    cursor.size = 48;
-    fonts = {
-      monospace = {
-        package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
-        name = "JetBrainsMono Nerd Font Mono";
-      };
-      sansSerif = {
-        package = pkgs.montserrat;
-        name = "Montserrat";
-      };
-      serif = {
-        package = pkgs.montserrat;
-        name = "Montserrat";
-      };
-      sizes = {
-        applications = 12;
-        terminal = 15;
-        desktop = 11;
-        popups = 12;
-      };
     };
   };
 
---
+++ nix/nixos/desktop/stylix.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+{
+  stylix = {
+    enable = true;
+    image = ../../../dots/wallpaper/platforms.png;
+    polarity = "dark";
+    opacity.terminal = 0.8;
+    cursor.package = pkgs.bibata-cursors;
+    cursor.name = "Bibata-Modern-Ice";
+    cursor.size = 48;
+    fonts = {
+      monospace = {
+        package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
+        name = "JetBrainsMono Nerd Font Mono";
+      };
+      sansSerif = {
+        package = pkgs.montserrat;
+        name = "Montserrat";
+      };
+      serif = {
+        package = pkgs.montserrat;
+        name = "Montserrat";
+      };
+      sizes = {
+        applications = 12;
+        terminal = 15;
+        desktop = 11;
+        popups = 12;
+      };
+    };
+  };
+}
