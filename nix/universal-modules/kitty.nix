ed2ae646a488b6593af0d5f9cef7629b0fddc981
whipplash <mattsnoopy2@gmail.com>
2024-10-21 00:40:38 -0400

    bell kitty

--- nix/universal-modules/kitty.nix
+++ nix/universal-modules/kitty.nix
@@ -12,9 +12,17 @@ in
         size = 16;
       };
       settings = {
+        # Window layout
         window_border_width = 1;
         window_padding_width = 5;
         window_margin_width = 1;
+        # Terminal Bell
+        enable_audio_bell = true;
+        visual_bell_duration = 0.0;
+        visual_bell_color = "none";
+        window_alert_on_bell = true;
+        bell_on_tab = "🔔 ";
+        command_on_bell = "none";
       };
       themeFile = "Catppuccin-Mocha";
     };
