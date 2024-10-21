9f058d2c456cd2a876340cf5c4a5e053c7d9dcc6
whipplash <mattsnoopy2@gmail.com>
2024-10-20 20:48:18 -0400

    Kitty

---
+++ nix/nixos/desktop/kitty.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+let
+  cfg = ../../../dots;
+in
+{
+  programs = {
+    kitty = {
+      enable = true;
+      font = pkgs.terminus-nerdfont;
+      settings = builtins.readFile "${cfg}/kitty/kitty.conf";
+    };
+  };
+}
