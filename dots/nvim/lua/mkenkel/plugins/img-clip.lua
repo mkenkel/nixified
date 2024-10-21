fbc9cc9bfa13128ee3bb35494eac5f356a39f180
whipplash <mattsnoopy2@gmail.com>
2024-10-21 03:09:29 -0400

    img-clip configuration

--- dots/nvim/lua/mkenkel/plugins/img-clip.lua
+++ dots/nvim/lua/mkenkel/plugins/img-clip.lua
@@ -2,9 +2,24 @@ return {
   "HakonHarnes/img-clip.nvim",
   event = "VeryLazy",
   opts = {
-    -- add options here
-    -- or leave it empty to use the default settings
+    prompt_for_file_name = false,
+    file_name = "%M-%D-%YYYY-at-%H-%M-%S",
+    extension = "avif", ---@type string
+    process_cmd = "convert - -quality 75 avif:-", ---@type string
+    -- drag and drop options
+    drag_and_drop = {
+      enabled = true, ---@type boolean | fun(): boolean
+      insert_mode = false, ---@type boolean | fun(): boolean
+    },
   },
+    -- filetype specific options
+  filetypes = {
+    markdown = {
+      -- encode spaces and special characters in file path
+      url_encode_path = true, ---@type boolean
+      template = "![$FILE_NAME]($FILE_PATH)", ---@type string
+  },
+
   keys = {
     -- suggested keymap
     { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
