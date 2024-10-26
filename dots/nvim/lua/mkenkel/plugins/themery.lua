fb1266e6ef022ae5b5c18ceaffa395c3ef0b28e8
whipplash <mattsnoopy2@gmail.com>
2024-10-26 15:37:42 -0400

    Colorscheme manager

---
+++ dots/nvim/lua/mkenkel/plugins/themery.lua
@@ -0,0 +1,43 @@
+return {
+  "zaldih/themery.nvim",
+  lazy = false,
+  config = function()
+    require("themery").setup({
+      themes = {
+        {
+          name = "Gruvbox dark",
+          colorscheme = "gruvbox",
+          before = [[
+          -- All this block will be executed before apply "set colorscheme"
+          vim.opt.background = "dark"
+          ]],
+        },
+        {
+          name = "Gruvbox light",
+          colorscheme = "gruvbox",
+          before = [[
+            vim.opt.background = "light"
+          ]],
+          after = [[-- Same as before, but after if you need it]]
+        },
+        {
+          name = "Gruvbox light",
+          colorscheme = "gruvbox",
+          before = [[
+            vim.opt.background = "light"
+          ]],
+          after = [[-- Same as before, but after if you need it]]
+        },
+        {
+          name = "Catpuccinn Mocha",
+          colorscheme = "catppuccin",
+          before = [[
+            vim.opt.background = "dark"
+          ]],
+          after = [[-- Same as before, but after if you need it]]
+        }
+      },
+      livePreview = true, -- Apply theme while picking. Default to true.
+    })
+  end
+}
