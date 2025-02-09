a0631eca29786cd58f6770a734ff38085db2c23b
whipplash <mattsnoopy2@gmail.com>
2025-02-09 12:26:39 -0500

    ly

--- nix/nixos/modules/login.nix
+++ nix/nixos/modules/login.nix
@@ -1,12 +1,20 @@
 { inputs, pkgs, ... }:
 {
   services = {
-    greetd = {
-      enable = true;
-      settings = {
-        default_session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --theme 'border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red;' --cmd Hyprland";
-          user = "greeter";
+    # greetd = {
+    #   enable = true;
+    #   settings = {
+    #     default_session = {
+    #       command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --theme 'border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red;' --cmd Hyprland";
+    #       user = "greeter";
+    #     };
+    #   };
+    # };
+    displayManager = {
+      ly = {
+        enable = true;
+        settings = {
+
         };
       };
     };
