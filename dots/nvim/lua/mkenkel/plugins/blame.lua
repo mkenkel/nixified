c94ec9d22a2d1ea32d02ce655d5b7e31989a8485
whipplash <mattsnoopy2@gmail.com>
2025-02-14 13:38:52 -0500

    one more

--- dots/nvim/lua/mkenkel/plugins/blame.lua
+++ dots/nvim/lua/mkenkel/plugins/blame.lua
@@ -1,11 +1,9 @@
 return {
-  {
-    "FabijanZulj/blame.nvim",
-    lazy = false,
-    config = function()
-      require('blame').setup {}
-    end,
-  },
+  "FabijanZulj/blame.nvim",
+  lazy = false,
+  config = function()
+    require('blame').setup {}
+  end,
   keys = {
     { '<leader>gb', '<cmd>BlameToggle<cr>', desc = 'Blame - Toggle' },
   },
