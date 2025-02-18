ac4153a91d011d3b75fe893853c787d73b2fae0f
whipplash <mattsnoopy2@gmail.com>
2025-02-17 20:12:34 -0500

    Networking

--- nix/nixos/desktop/default.nix
+++ nix/nixos/desktop/default.nix
@@ -107,8 +107,23 @@ in
     };
   };
 
-  networking.hostName = "${hostname}";
-  networking.wireless.enable = true;
+  networking = {
+    hostName = "${hostname}";
+    wireless.enable = true;
+
+    # interfaces = {
+    #   enp5s0.ipv4.addresses = [
+    #     {
+    #       address = "192.168.5.9";
+    #       prefixLength = 24;
+    #     }
+    #   ];
+    # };
+    # defaultGateway = {
+    #   address = "192.168.10.1";
+    #   interface = "enp5s0";
+    # };
+  };
 
   users.users.${user} = {
     isNormalUser = true;
