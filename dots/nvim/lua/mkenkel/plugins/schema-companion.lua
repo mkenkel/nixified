58d439fd5c23959ec39dfacadb0a6702eb7ca824
whipplash <mattsnoopy2@gmail.com>
2025-03-11 19:32:42 -0400

    picker

--- dots/nvim/lua/mkenkel/plugins/schema-companion.lua
+++ dots/nvim/lua/mkenkel/plugins/schema-companion.lua
@@ -5,7 +5,6 @@ return {
     { "nvim-telescope/telescope.nvim" },
   },
   config = function()
-    local schema_companion = require("telescope").extensions.yaml_schema
     require("schema-companion").setup({
       -- if you have telescope you can register the extension
       enable_telescope = true,
@@ -26,6 +25,7 @@ return {
         },
       }
     })
-    vim.keymap.set("n", "<leader>sp", schema_companion.select_schema(), { desc = "Select Schema manually." })
+    vim.keymap.set("n", "<leader>sp", require("telescope").extensions.yaml_schema.select_schema(),
+      { desc = "Select Schema manually." })
   end,
 }
