80b1ccd5847c2b4811ac19b4384617e65641580b
whipplash <mattsnoopy2@gmail.com>
2025-03-11 17:56:45 -0400

    ansible 2.9 definitely EOL - Subbing for lint instead

--- dots/nvim/lua/mkenkel/plugins/lsp/lspconfig.lua
+++ dots/nvim/lua/mkenkel/plugins/lsp/lspconfig.lua
@@ -147,10 +147,10 @@ return {
           settings = {
             yaml = {
               schemas = {
-                kubernetes = "k8s-*.yaml",
+                kubernetes = "*.k8s.yaml",
                 ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
                 ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-                ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/**/*.{yml,yaml}",
+                ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible-lint-config.json"] = "roles/tasks/**/*.{yml,yaml}",
                 ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
                 ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
                 ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
