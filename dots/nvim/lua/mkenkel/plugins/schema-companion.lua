a5822ee12013f9e58e9b74094ae5449c5ab143eb
whipplash <mattsnoopy2@gmail.com>
2025-03-11 22:55:53 -0400

    OH manually select from these when kube matches, I see I see

--- dots/nvim/lua/mkenkel/plugins/schema-companion.lua
+++ dots/nvim/lua/mkenkel/plugins/schema-companion.lua
@@ -12,10 +12,34 @@ return {
         require("schema-companion.matchers.kubernetes").setup({ version = "master" }),
       },
       schemas = {
-        -- {
-        --   name = "Kubernetes master",
-        --   uri = "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/master-standalone-strict/all.json",
-        -- },
+        {
+          name = "Kubernetes master",
+          uri = "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/master-standalone-strict/all.json",
+        },
+        {
+          name = "Kubernetes v1.27",
+          uri =
+          "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.27.16-standalone-strict/all.json",
+        },
+        {
+          name = "Kubernetes v1.28",
+          uri =
+          "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.28.12-standalone-strict/all.json",
+        },
+        {
+          name = "Kubernetes v1.29",
+          uri =
+          "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.29.7-standalone-strict/all.json",
+        },
+        {
+          name = "Kubernetes v1.30",
+          uri =
+          "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.30.3-standalone-strict/all.json",
+        },
+        {
+          name = "Gitlab CI",
+          uri = "https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json",
+        },
       }
     })
     vim.keymap.set("n", "<leader>ss",
