52e28c476037d6ec11bdc4ab2410fddbdc582d75
whipplash <mattsnoopy2@gmail.com>
2025-03-13 14:46:29 -0400

    Cleanup

--- dots/nvim/lua/mkenkel/plugins/nvim-tree.lua
+++ dots/nvim/lua/mkenkel/plugins/nvim-tree.lua
@@ -1,6 +1,13 @@
 return {
   "nvim-tree/nvim-tree.lua",
   dependencies = "nvim-tree/nvim-web-devicons",
+  keys = {
+    { "n", "<leader>ef", "<cmd>NvimTreeToggle<CR>",         { desc = "Toggle file explorer" } },
+    { "n", "<leader>ed", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" } },
+    { "n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>",       { desc = "Collapse file explorer" } },
+    { "n", "<leader>er", "<cmd>NvimTreeRefresh<CR>",        { desc = "Refresh file explorer" } },
+
+  },
   config = function()
     -- disable netrw at the very start of your init.lua
     vim.g.loaded_netrw = 1
@@ -37,13 +44,5 @@ return {
         ignore = false,
       },
     })
-
-    local keymap = vim
-        .keymap                                                                                                         -- Initializing keymaps relative to plugins to keep things neater.
-
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })                         -- toggle file explorer
-    keymap.set("n", "<leader>ed", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })                     -- collapse file explorer
-    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })                       -- refresh file explorer
   end
 }
