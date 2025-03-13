b610e94359a648fd6cde24dde7c7de6c8e743b91
whipplash <mattsnoopy2@gmail.com>
2025-03-13 13:22:19 -0400

    Jesus I'm actually reading docs now...

--- dots/nvim/lua/mkenkel/plugins/autopairs.lua
+++ dots/nvim/lua/mkenkel/plugins/autopairs.lua
@@ -3,27 +3,15 @@ return {
   dependencies = "hrsh7th/nvim-cmp",
   event = "InsertEnter",
 
-  config = function()
-    -- import autopairs
-    local autopairs = require("nvim-autopairs")
+  opts = {
+    check_ts = true,                      -- Check treesitter
+    ts_config = {
+      lua = { "string" },                 -- Don't add pairs in lua string treesitter nodes
+      javascript = { "template_string" }, -- Dont add pairs in javascript template_string treesitter nodes
+      java = false,                       -- Don't check treesitter when in java.
+    },
 
-    -- configure autopairs
-    autopairs.setup({
-      check_ts = true,                      -- Check treesitter
-      ts_config = {
-        lua = { "string" },                 -- Don't add pairs in lua string treesitter nodes
-        javascript = { "template_string" }, -- Dont add pairs in javascript template_string treesitter nodes
-        java = false,                       -- Don't check treesitter when in java.
-      },
-    })
-
-    -- import nvim-autopairs completion functionality
-    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-
-    -- Import nvim-cmp plugin (Completions plugin)
-    local cmp = require("cmp")
-
-    -- Make autopairs and completion work together
-    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-  end,
+  },
+  -- Make autopairs and completion work together
+  require("cmp").event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done()),
 }
