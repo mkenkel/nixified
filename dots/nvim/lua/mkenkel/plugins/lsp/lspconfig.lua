9519c2b361d4561c2951ccecd911a1550a5aa4bb
whipplash <mattsnoopy2@gmail.com>
2025-03-13 09:06:46 -0400

    Alright hard truth... I'm not even using these keybinds

--- dots/nvim/lua/mkenkel/plugins/lsp/lspconfig.lua
+++ dots/nvim/lua/mkenkel/plugins/lsp/lspconfig.lua
@@ -15,26 +15,6 @@ return {
     local mason_lspconfig = require("mason-lspconfig")
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-
-    local keymap = vim.keymap
-    vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-      -- Buffer local mappings. See `:help vim.lsp.*` for documentation on any of the below functions
-      keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", { desc = "Show LSP references" }),
-      keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" }),
-      keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", { desc = "Show LSP definitions" }),
-      keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", { desc = "Show LSP implementations" }),
-      keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", { desc = "Show LSP type definitions" }),
-      keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "See available code actions" }),
-      keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Smart rename" }),
-      keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", { desc = "Show buffer diagnostics" }),
-      keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show line diagnostics" }),
-      keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" }),
-      keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" }),
-      keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show documentation for what is under cursor" }),
-      keymap.set("n", "<leader>rs", ":LspRestart<CR>", { desc = "Restart LSP" }),
-    })
-
     -- used to enable autocompletion (assign to every lsp server config)
     local capabilities = cmp_nvim_lsp.default_capabilities()
 
