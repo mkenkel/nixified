46c53776fc44819a78064a233dc331a89b94d77c
whipplash <mattsnoopy2@gmail.com>
2025-03-16 20:36:17 -0400

    vscodium

--- dots/nvim/lua/mkenkel/plugins/markdown.lua
+++ dots/nvim/lua/mkenkel/plugins/markdown.lua
@@ -1,6 +1,7 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
-  after = { 'nvim-treesitter' },
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
   requires = { 'echasnovski/mini.nvim', opt = true }, -- if you use the mini.nvim suite
   opts = {
     completions = {
