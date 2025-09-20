9e1db927673fe00b38cc14b1dd325852f158b3e0
whipplash <mattsnoopy2@gmail.com>
2025-09-20 11:54:04 -0400

    fuzzel to get going

--- nix/nixos/desktop/river.nix
+++ nix/nixos/desktop/river.nix
@@ -5,14 +5,12 @@
     enable = true;
     xwayland.enable = true;
     extraConfig = ''
-      # Set default terminal (example: foot)
-      set-terminal kitty
-
       # Monitor Configuration
       exec wlr-randr --output DP-3 --mode 3840x2160@144.00Hz
 
       # Example keybindings
       bindsym Mod1+Return spawn kitty
+      bindsym Mod1+D spawn fuzzel 
       bindsym Mod1+Q close
       bindsym Mod1+Shift+Q exit
       bindsym Mod1+J focus next
