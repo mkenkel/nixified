f062a81819a6254aebf21abf875ede9b52df3268
Matthew Kenkel <mattsnoopy2@gmail.com>
2026-01-31 16:17:07 +0000

    wsl support

---
+++ nix/wsl/flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+  };
+
+  outputs = { self, nixpkgs, nixos-wsl, ... }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          nixos-wsl.nixosModules.default
+          {
+            system.stateVersion = "25.05";
+            wsl.enable = true;
+          }
+        ];
+      };
+    };
+  };
+}
+
