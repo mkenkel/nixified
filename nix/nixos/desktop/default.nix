bb5e185b97197c4449a1a41994bdfc16936a9f3e
whipplash <mattsnoopy2@gmail.com>
2026-02-26 22:57:06 -0500

    full steam ahead without hypr anything

--- nix/nixos/desktop/default.nix
+++ nix/nixos/desktop/default.nix
@@ -128,12 +128,12 @@ in
 
   programs = {
     hyprland = {
-      enable = true;
+      enable = false;
       # set the flake package
       package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
       # make sure to also set the portal package, so that they are in sync
-      portalPackage =
-        inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+      # portalPackage =
+      # inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     };
   };
 
@@ -218,7 +218,7 @@ in
     displayManager = {
       sessionPackages = with pkgs; [
         river-classic
-        hyprland
+        # hyprland
       ];
     };
     pipewire = {
