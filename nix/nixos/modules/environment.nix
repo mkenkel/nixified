cd0dee2ae031b9debdcafaea564e9211e3c7f490
whipplash <mattsnoopy2@gmail.com>
2026-09-12 14:41:31 -0400

    whoops

--- nix/nixos/modules/environment.nix
+++ nix/nixos/modules/environment.nix
@@ -8,8 +8,8 @@
       QT_QPA_PLATFORM = "wayland";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = 1;
       SDL_VIDEODRIVER = "wayland";
-      XDG_CURRENT_DESKTOP = "river";
-      XDG_SESSION_DESKTOP = "river";
+      XDG_CURRENT_DESKTOP = "niri";
+      XDG_SESSION_DESKTOP = "niri";
       XDG_SESSION_TYPE = "wayland";
     };
     interactiveShellInit = ''
